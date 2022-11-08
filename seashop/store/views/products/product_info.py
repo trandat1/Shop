c37afe220b6eb6cb8b6056bd3e477777b540a4af
trandat1 <GiaVien2002@gmail.com>
2022-11-08 08:29:01 +0700

    commit	modified: seashop/db.sqlite3 commit seashop/store/templates/products/info.html commit	modified:   seashop/store/views/products/product_info.py

--- seashop/store/views/products/product_info.py
+++ seashop/store/views/products/product_info.py
@@ -11,10 +11,16 @@ class cart(View):
         # request.session.clear()
         prt = Product.get_product_by_name(name)
         pro = Productdetail. get_size_product(name)
+
+        user = request.session.get('customer_id')
+        if not user:
+            user = None
+
         template = loader.get_template('products/info.html')
         context = {
             'prt': prt,
-            'pro': pro
+            'pro': pro,
+            'user': user
         }
         return HttpResponse(template.render(context, request))
 
